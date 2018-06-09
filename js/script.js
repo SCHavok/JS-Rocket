7295ea41d1c4a0a897ded66d1c4684d22d149fea
SCHavok <joshuajamestate@gmail.com>
2018-06-09 17:25:42 +1000

    Latest Version

---
+++ js/script.js
@@ -0,0 +1,27 @@
+	var car = {
+		make: "VW",
+		type: "Polo",
+		colour: "blue",
+		isTurnedOn: false,
+		numberOfWheels: 4,
+		seats: [
+			"seat 1", 
+			"seat 2", 
+			"seat 3", 
+			"seat 4"
+		],
+		turnOn: function(){
+			this.isTurnedOn = true;
+		},
+		switchCar: function(isOn) {
+			if (isOn == true) {
+				this.isTurnedOn = true;
+			} else {
+				this.isTurnedOn = false;
+			}
+		},
+		fly: function(){
+			alert("Fly");
+		},
+	};
+	console.log('hello there friends!');
